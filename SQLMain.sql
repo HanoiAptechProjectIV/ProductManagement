d2ad3e151e5af8350ddc52101102adcdc9d2e38e
Project IV <75621439+ProjectIV@users.noreply.github.com>
2020-12-31 09:29:58 +0700

    Update SQLMain.sql

--- SQLMain.sql
+++ SQLMain.sql
@@ -69,20 +69,26 @@ create table OrderDetail (
 )
 
 insert into Category(name, description) values(N'category 1', N'description')
+, (N'category 2', N'description')					   
 
-insert into Brand (name, logo, manufacturer, description) values(N'brand 1', 'tesla.png', 'made in china', N'description')
+insert into Brand (name, logo, manufacturer, description) values
+(N'brand 1', 'tesla.png', 'made in china', N'description')
+, (N'brand 2', 'tesla.png', 'made in usa', N'description')					   
 
 insert into Product(name, price, quantity, image, description, date_added, category_id, brand_id)
 values(N'product 1', 100000, 50, 'product.png', N'good quality', '2020/12/27 12:30:00', 1, 1)
 , (N'product 2', 200000, 30, 'product.png', N'good quality', '2020/12/27 12:15:00', 1, 1)					   
 
 insert into [User](username, password, name, gender, phone, email, address)
-values('customer', '123', 'John Doe', 1, '098 765 4321', 'johndoe@customer.com', N'Hà Nội')
+values('customer', '123', 'John Doe', 1, '098 765 4321', 'johndoe@customer.com', N'Ha Noi')
+, ('customer1', '123', 'Anonymous', 0, '012 345 6789', 'anonymous@customer.com', N'New Delhi')	     
 
 insert into [Order](created_time, payment_time, amount, [user_id])
 values ('12/26/2020 15:00:00', '01/01/2021 15:28:00', 2200000, 1)
-, ('01/01/2021 17:00:00', '01/01/2021 19:28:00', 100000, 1)							 
+, ('01/01/2021 17:00:00', '01/01/2021 19:28:00', 100000, 1)	
+, ('01/01/2021 15:00:00', '01/01/2021 20:12:00', 300000, 2)						 
 
 insert into OrderDetail(status, product_id, order_id, purchased_quantity)
 values('success transaction', 1, 1, 2), ('success transaction', 2, 1, 10)
-, ('success transaction', 1, 2, 1)										 
+, ('success transaction', 1, 2, 1), ('success transaction', 2, 3, 1)
+, ('success transaction', 1, 3, 1)							 
